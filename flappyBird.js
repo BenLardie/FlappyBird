98d73839f3c03a8f705715f20a1cd5c64de7a704
Ben Lardie <benlardie@gmail.com>
2019-08-01 20:50:23 -0400

    added highscore

--- flappyBird.js
+++ flappyBird.js
@@ -22,6 +22,7 @@ let bY = 150;
 const gravity = 1.5;
 
 let score = 0;
+let highScore = 0;
 
 const fly = new Audio();
 const scor = new Audio();
@@ -31,43 +32,46 @@ scor.src = 'sounds/score.mp3';
 
 const pipe = [];
 pipe[0] = {
-    x: cvs.width,
-    y: 0
+  x: cvs.width,
+  y: 0,
 };
 
-
 const moveUp = () => {
-    bY -= 25;
-    fly.play();
+  bY -= 25.5;
+  fly.play();
 };
 
 document.addEventListener('keydown', moveUp);
 
 const draw = () => {
-    ctx.drawImage(bg, 0, 0);
+  ctx.drawImage(bg, 0, 0);
 
-    for (let i = 0; i < pipe.length; i++) {
-        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
-        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+  for (let i = 0; i < pipe.length; i++) {
+    ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+    ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
 
-        pipe[i].x--;
+    pipe[i].x--;
 
-        if (pipe[i].x === 125) {
-            pipe.push({
-                x: cvs.width,
-                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
-            });
-        }
+    if (pipe[i].x === 125) {
+      pipe.push({
+        x: cvs.width,
+        y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height,
+      });
+    }
 
-        if (bX + flappyBird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width &&
-            (bY <= pipe[i].y + pipeNorth.height || bY + flappyBird.height >= pipe[i].y + constant) ||
-            bY + flappyBird.height >= cvs.height - fg.height) {
-            location.reload()
+    if (bX + flappyBird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width &&
+        (bY <= pipe[i].y + pipeNorth.height || bY + flappyBird.height >= pipe[i].y + constant) ||
+        bY + flappyBird.height >= cvs.height - fg.height) {
+        location.reload()
         }
 
         if (pipe[i].x === 5) {
-            score++;
+      score++;
             scor.play();
+        }
+
+        if (score > highScore){
+            highScore = score;
         }
     }
     constant = pipeNorth.height + gap;
@@ -79,7 +83,7 @@ const draw = () => {
 
     ctx.fillStyle = "#000";
     ctx.font = "20px Verdana";
-    ctx.fillText("Score : " + score, 10, cvs.height - 20);
+    ctx.fillText("Score : " + score + "High Score: " + highScore, 10, cvs.height - 20);
 
     requestAnimationFrame(draw);
 }
